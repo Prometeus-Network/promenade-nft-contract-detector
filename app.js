0aa664d8ccf8d4e6a49f4afc47b940134dd1fc94
Aleksei Dmitriev <alexey@ioncloud.ru>
2021-10-03 10:46:03 +0200

    ci: add mainnet

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const Utils = require('./services/utils')
 const TrackContractDeployment = require('./services/contractDeploymentTracker')
 const healthcheck = require('./healthcheck')
 
-const NODEINDEX = parseInt(process.env.NODEINDEX)
-
 const connect = () => {
   const uri = process.env.DB_URL
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,13 +12,10 @@ const connect = () => {
   db.on('error', console.error.bind(console, 'connection error:'))
   db.once('open', function () {
     console.log('nft contract tracker has been started')
-    if (NODEINDEX != 10) {
-      console.log('tracking contract deployment')
-      TrackContractDeployment()
-    } else {
-      console.log('tracking names and symbols')
-      Utils.trackNameAndSymbol()
-    }
+    console.log('tracking contract deployment')
+    TrackContractDeployment()
+    console.log('tracking names and symbols')
+    Utils.trackNameAndSymbol()
     healthcheck()
   })
 }
